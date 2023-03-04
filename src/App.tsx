eb304478e609d553dd9fca7af9d9451b7a33e7f5
Richardrodri26 <richardmanuel811@gmail.com>
2023-03-04 11:11:18 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className='card'>
+        <div className='card-qr'>
+          <img src="/images/image-qr-code.png" alt="qr" />
+        </div>
+        <div className='card-text'>
+          <p className="title">Improve your front-end skills by building projects</p>
+          <p className="description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
